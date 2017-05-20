0a9579bc332c4a0c4e09924c83e7f2f9a8212c02
WilliamsGon <williamseduardo93@gmail.com>
2017-05-20 14:37:59 -0400

    Se agregó una Validación al cerrar el Sistema Principal.

--- AranduraC/MainWindow.xaml.cs
+++ AranduraC/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using System.Windows.Media.Animation;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace AranduraC
 {
@@ -22,6 +23,7 @@ namespace AranduraC
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private bool _shutdown;
         public MainWindow()
         {
             InitializeComponent();
@@ -45,6 +47,40 @@ namespace AranduraC
 
             //AranduraC.Controles.ctrlLogin ctrlLogin = new Controles.ctrlLogin();
             MainFrame.Navigate(new Controles.pageLogin(this.MainFrame));
+        }
+        //PROCESO QUE SE EJECUTA CUANDO LA HERRAMIENTA SE ESTÁ CERRANDO
+        private void MetroWindow_Closed(object sender, EventArgs e)
+        {
+            foreach (Window win in App.Current.Windows)
+            {
+                if (!win.IsFocused/* && win.Tag.ToString() == "mdi_child"*/)
+                {
+                    win.Close();
+                }
+            }
+        }
+        //PROCESO QUE SE EJECUTA CUANDO SE QUIERE CERRAR LA HERRAMIENTA
+        private async void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = !_shutdown;
+            if (_shutdown) return;
+
+            var mySettings = new MetroDialogSettings()
+            {
+                AffirmativeButtonText = "Salir",
+                NegativeButtonText = "Cancelar",
+                AnimateShow = true,
+                AnimateHide = false
+            };
+
+            var result = await this.ShowMessageAsync("¿Salir de Arandura?",
+                "¿Está seguro que desea salir de Arandura?",
+                MessageDialogStyle.AffirmativeAndNegative, mySettings);
+
+            _shutdown = result == MessageDialogResult.Affirmative;
+
+            if (_shutdown)
+                Application.Current.Shutdown();
         }
     }
 }
